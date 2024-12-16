94ffb4b8c0117dfbbd658d0198d40e56ac7c4d99
WilsonD09 <wilsond34@nycstudents.net>
2024-12-16 12:31:26 -0500

    s

---
+++ app/JS/deck.js
@@ -0,0 +1,94 @@
+const deck = [
+  {
+    number: "Ace",
+    suit: "Clubs",
+    imageURL: "https://example.com/images/ace_of_clubs.png",
+    altText: "Ace of Clubs",
+  },
+  {
+    number: "2",
+    suit: "Clubs",
+    imageURL: "https://example.com/images/2_of_clubs.png",
+    altText: "2 of Clubs",
+  },
+  {
+    number: "3",
+    suit: "Clubs",
+    imageURL: "https://example.com/images/3_of_clubs.png",
+    altText: "3 of Clubs",
+  },
+  {
+    number: "4",
+    suit: "Clubs",
+    imageURL: "https://example.com/images/4_of_clubs.png",
+    altText: "4 of Clubs",
+  },
+  {
+    number: "5",
+    suit: "Clubs",
+    imageURL: "https://example.com/images/5_of_clubs.png",
+    altText: "5 of Clubs",
+  },
+  {
+    number: "6",
+    suit: "Clubs",
+    imageURL: "https://example.com/images/6_of_clubs.png",
+    altText: "6 of Clubs",
+  },
+  {
+    number: "7",
+    suit: "Clubs",
+    imageURL: "https://example.com/images/7_of_clubs.png",
+    altText: "7 of Clubs",
+  },
+  {
+    number: "8",
+    suit: "Clubs",
+    imageURL: "https://example.com/images/8_of_clubs.png",
+    altText: "8 of Clubs",
+  },
+  {
+    number: "9",
+    suit: "Clubs",
+    imageURL: "https://example.com/images/9_of_clubs.png",
+    altText: "9 of Clubs",
+  },
+  {
+    number: "10",
+    suit: "Clubs",
+    imageURL: "https://example.com/images/10_of_clubs.png",
+    altText: "10 of Clubs",
+  },
+  {
+    number: "Jack",
+    suit: "Clubs",
+    imageURL: "https://example.com/images/jack_of_clubs.png",
+    altText: "Jack of Clubs",
+  },
+  {
+    number: "Queen",
+    suit: "Clubs",
+    imageURL: "https://example.com/images/queen_of_clubs.png",
+    altText: "Queen of Clubs",
+  },
+  {
+    number: "King",
+    suit: "Clubs",
+    imageURL: "https://example.com/images/king_of_clubs.png",
+    altText: "King of Clubs",
+  },
+  {
+    number: "Ace",
+    suit: "Diamonds",
+    imageURL: "https://example.com/images/ace_of_diamonds.png",
+    altText: "Ace of Diamonds",
+  },
+  {
+    number: "2",
+    suit: "Diamonds",
+    imageURL: "https://example.com/images/2_of_diamonds.png",
+    altText: "2 of Diamonds",
+  },
+  // Repeat for all ranks and suits
+];
+export { deck };
