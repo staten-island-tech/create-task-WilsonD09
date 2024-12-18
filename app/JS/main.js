26c024efd00f1511a793d9b8390f8d5cd5ca38c4
WilsonD09 <wilsond34@nycstudents.net>
2024-12-18 12:30:23 -0500

    d

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,1 +1,14 @@
 import "../CSS/style.css";
+import { deck } from "./deck";
+
+console.log(deck);
+function createCards(list) {
+  list.forEach((el) => {
+    document.querySelector(".container").insertAdjacentHTML(
+      "beforeend",
+      `<div class="card" id=""><h3>${el.number} of ${el.suit}</h3>
+            <img class="img" src=${el.imageURL} alt="${el.altText}">`
+    );
+  });
+}
+createCards(deck);
