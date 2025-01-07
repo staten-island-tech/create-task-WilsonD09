d288439a8eca15242dab94fa2927215b440225e0
WilsonD09 <158211754+WilsonD09@users.noreply.github.com>
2025-01-07 08:36:24 -0500

    x

--- app/JS/main.js
+++ app/JS/main.js
@@ -2,14 +2,14 @@ import "../CSS/style.css";
 import { deck } from "./deck";
 
 console.log(deck);
+
 function createCards(list) {
-  list.forEach((el) => {
-    document.querySelector(".container").insertAdjacentHTML(
-      "beforeend",
-      `<div class="card" id=""><h3>${el.number} of ${el.suit}</h3>
-            <img class="img" src=${el.imageURL} alt="${el.altText}">`
-    );
+  const cardList = [];
+  list.forEach((el, index) => {
+    cardList.push(`<div class="card" id="card-${index}"><h3>${el.number} of ${el.suit}</h3>
+            <img class="img" src=${el.imageURL} alt="${el.altText}"></div>`);
   });
+  console.log(cardList);
 }
 createCards(deck);
 
